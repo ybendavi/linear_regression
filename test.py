b72f853329d13dee1a5b180d9192ca52f475a0bc
ybendavi <44405448+ybendavi@users.noreply.github.com>
2024-11-13 22:20:35 +0100

    Update test.py
    Correction de l'algorithme, il fallait normaliser la donnee independante lors du calcule du nouveau teta1 pour ne pas se retrouver avec des valeurs trop hautes, reste a coder une fonction qui adapte le learning rate en fonction des marges d'erreur

--- test.py
+++ test.py
@@ -4,7 +4,11 @@ import random
 #formule pour renvoyer un point de la courbe en fonction du kilometrage
 def predic(mileage, teta0, teta1):
     return (teta0 + (teta1 * mileage))
-
+#formule de normalisation
+def normalize(number, tab):
+    n_max = max(tab)
+    n_min = min(tab)
+    return (number - n_min) / (n_max - n_min)
 def calculat_err_m(m, mileage, price, teta0, teta1, teta):
     i = 0
     err_sum = 0
@@ -12,14 +16,17 @@ def calculat_err_m(m, mileage, price, teta0, teta1, teta):
     #ici il faut calculer la somme des erreurs (la difference entre les prix estimes et ceux dans le dataset puis ce sera la difference entre les precedent et les nouveauc)
     while i < m:
         if teta == 0:
-            err_sum += predic(mileage[i], teta0, teta1) - price[i]
+            err_sum += (predic(mileage[i], teta0, teta1) - price[i])
         else:
-            err_sum += (predic(mileage[i], teta0, teta1) - price[i]) * mileage[i]
+            # application de la formule pour le teta1, on multiplie le resultat par une normalisation de la donnee traitee (la transformer en une valeur entre 0 et 1) parce que plus elle est haute plus elle influencera la courbe
+            err_sum += ((predic(mileage[i], teta0, teta1) - price[i]) * normalize(mileage[i], mileage))
+            #print(normalize(mileage[i], mileage))
         #print("Prediction", predic(mileage[i], teta0, teta1), "/Prix", price[i])
         i += 1
-    err_m = err_sum * 1/m
-    #print("Somme des erreurs:", err_sum)
+    err_m = err_sum * (1 / m)
+    #print("Somme des erreurs:", err_sum, "m:", m)
     return err_m
+
 def linear_regression():
     price = []
     mileage = []
@@ -36,15 +43,21 @@ def linear_regression():
     m = len(mileage)
     teta0 = 0
     teta1 = 0
-    learning_rate = 0.1
+    #coder une fonction pour ajuster le learning rate
+    learning_rate = 0.000041 
     i = 0
-    while i < 10: 
+    errs = []
+    while i < 10000: 
         err_m = calculat_err_m(m, mileage, price, teta0, teta1, 0)
+        errs.append(err_m)
         print("Moyenne des erreurs", err_m)
-        teta0 -= learning_rate * calculat_err_m(m, mileage, price, teta0, teta1, 0)
-        teta1 -= learning_rate * calculat_err_m(m, mileage, price, teta0, teta1, 1)
-        print("teta0:", teta0, ",testa1:", teta1)
+        old0 = teta0
+        old1 = teta1
+        teta0 -= (learning_rate * calculat_err_m(m, mileage, price, old0, old1, 0))
+        teta1 -= (learning_rate * calculat_err_m(m, mileage, price, old0, old1, 1))
+        print("teta0:", teta0, ",teta1:", teta1)
         i += 1
+    print(min(errs, key=abs))
     #il faut que l'erreur totale soit la plus petite possible
     #definir un learning rate a ajuster
     #appliquer la formule pour ajuster teta0
