42f526790c6688767799bfdaec8d92ff60de5147
Yohan BENDAVID <ybendavi@paul-f4Br3s1.clusters.42paris.fr>
2024-11-15 19:14:10 +0100

    ajout de la standardisation des donnees mais ne fonctionne pas encore

--- ft_linear_regression.py
+++ ft_linear_regression.py
@@ -1,35 +1,49 @@
 import pandas as pd
+import math 
 
 # Fonction de prediction
 def estimatePrice(mileage, teta0, teta1):
     return (teta0 + (teta1 * mileage))
 
+def printEstimatedPrice(column, tetas):
+    return (tetas[0] + (teta[1] * mileage))
+
 def normalize(number, n_min, n_max):
     return (number - n_min) / (n_max - n_min)
 
-def normalize_column(column):
-    n_min = column.min()
-    n_max = column.max()
-    return (column - n_min) / (n_max - n_min)
+def calculate_square(column, mean):
+    return (pow(column - mean, 2))
+
+#Fonction pour calculer la variance qui est la moyenne des carrés des écarts de chaque valeur par rapport à la moyenne
+def calculate_v(mean, column, size):
+    new_col = column.apply(calculate_square, args=(mean,))
+    square_mean = new_col.sum() / size
+    return (square_mean)
+
+#Fonction pour standardiser les valeurs
+def standardize_column(column, standard_deviation, mean):
+    return ((column - mean) / standard_deviation )
+
 #Fonctions pour calculer les erreurs correspond a la partie estimatePrice(mileage[i]) − price[i] de la formule
-def calculate_err_0(line, tmp0, tmp1):
+def calculate_err_0(line, tmp0, tmp1, test):
+    if test == 0:
+        return (estimatePrice(line["s_km"], tmp0, tmp1) - line["s_price"])
+    else:
         return (estimatePrice(line["km"], tmp0, tmp1) - line["price"])
 
-# Pour le teta 1 il faut multiplie la marge d'erreur par le mileage normalise par a l'influence qu'il a sur la courbe
-def calculate_err_1(line, tmp0, tmp1, min, max):
-        return ((estimatePrice(line["km"], tmp0, tmp1) - line["price"]) * normalize(line["km"], min, max))
+# Pour le teta 1 il faut multiplier la marge d'erreur par le mileage par rapport a l'influence qu'il a sur la courbe
+def calculate_err_1(line, tmp0, tmp1):
+    return ((estimatePrice(line["s_km"], tmp0, tmp1) - line["s_price"]) * line["s_km"])
 
 def calcute_sum_m(tmp0, tmp1, datas):
     tab0 = []
     tab1 = []
-    vmin = datas["km"].min()
-    vmax = datas["km"].max()
     # Je recupere le nombre de donnees
     m = datas.shape[0]
 
     for index, row in datas.iterrows():
-        tab0.append(calculate_err_0(row, tmp0, tmp1))
-        tab1.append(calculate_err_1(row, tmp0, tmp1, vmin, vmax))
+        tab0.append(calculate_err_0(row, tmp0, tmp1, 0))
+        tab1.append(calculate_err_1(row, tmp0, tmp1))
     # J'applique le 1/m * somme des erreurs
     sums_m = []
     try:
@@ -38,33 +52,59 @@ def calcute_sum_m(tmp0, tmp1, datas):
         print("Nombre en question:", sum(tab0), "/nError:", e)
     sums_m.append(sum(tab1) * (1 / m))
     return (sums_m)
+
+# Ici je calcule la mean absolute error pour mes comparaisons d'erreurs car si j'utilise l'erreur moyenne les negatifs et positif vont s'annuler
+def calculate_mae(tmp0, tmp1, datas):
+    tab0 = []
+    m = datas.shape[0]
+    for index, row in datas.iterrows():
+        tab0.append(abs(calculate_err_0(row, tmp0, tmp1, 1)))
+    #print("sum:", sum(tab0), "   m:", m, "    1/m:", 1 / m)
+    #print("tab:", tab0)
+    err_m = sum(tab0) * (1 / m)
+    return (err_m)
     
 def ft_linear_regression(datas, teta0, teta1):
     tmp0 = teta0
     tmp1 = teta1
-    normalized_datas = datas.apply(normalize_column)
+    new0 = tmp0
+    new1 = tmp1
     #print(normalized_datas)
+    #On va standardiser les valeurs pour ca on calcule l'ecart type
+    mean_mileage = datas["km"].sum() /  len(datas["km"])
+    standard_deviation_mileage = math.sqrt(calculate_v(mean_mileage, datas["km"], len(datas["km"])))
+    datas["s_km"] = datas["km"].apply(standardize_column, args=(standard_deviation_mileage, mean_mileage))
+    mean_price= datas["price"].sum() /  len(datas["price"])
+    standard_deviation_price = math.sqrt(calculate_v(mean_price, datas["price"], len(datas["price"])))
+    datas["s_price"] = datas["price"].apply(standardize_column, args=(standard_deviation_price, mean_price))
     # On defini un learning rate de depart pour faire des pas plus ou moins grand sur la courbe d'erreur
-    learning_rate = 0.000041
-    nb_iterations = 1000
-    weaker_err = calcute_sum_m(tmp0, tmp1, datas)
+    learning_rate = 0.006
+    nb_iterations = 50
+    weaker_err = calculate_mae(tmp0, tmp1, datas)
+    print("werror", weaker_err)
     #print ("datas:", datas)
     i = 0
+    savedi = i
     while i < nb_iterations:
 
         # Il faut ici recuperer la moyenne de la somme des erreurs pour teta0 et teta1
         sums_m = calcute_sum_m(tmp0, tmp1, datas)
         
-        #print("Moyenne d'etteur:", sums_m[0])
+        # print("Moyenne d'erreur:", sums_m[0])
         # On peu maintenant recalculer teta0 et teta1
         tmp0 = tmp0 - learning_rate * sums_m[0]
         tmp1 = tmp1 - learning_rate * sums_m[1] 
-        if abs(sums_m[0]) < abs(weaker_err[0]):
-            weaker_err = sums_m
+        new_err = calculate_mae(tmp0, tmp1, datas)
+        #print("new err", new_err)
+        if weaker_err > new_err:
+            weaker_err = new_err
+            new0 = tmp0
+            new1 = tmp1
+            savedi = i
         i += 1
-    print(weaker_err[0], " ", i)
-    print("prix estime:", estimatePrice(82029, weaker_err[0], weaker_err[1]))
-
+    print(weaker_err, " ", savedi)
+    datas["estimated price"] = datas["km"].apply(estimatePrice, args=(new0, new1))
+    print(datas)
 def main():
     # Recuperation des donnees dans le csv
     datas = None
