0a19c64bc394353fc58a113273bafda8e921141e
Yohan BENDAVID <ybendavi@paul-f4Br4s4.clusters.42paris.fr>
2024-11-16 19:19:43 +0100

    le modele fonctionne mieux, a ameliorer, j'ai garde les valeurs standardise uniquement pour la valeur qu'on multiplie dans le recualcule de teta1

--- ft_linear_regression.py
+++ ft_linear_regression.py
@@ -46,13 +46,13 @@ def destandardise_teta0(teta0, standard_deviation, m, teta1_d):
 #Fonctions pour calculer les erreurs correspond a la partie estimatePrice(mileage[i]) − price[i] de la formule
 def calculate_err_0(line, tmp0, tmp1, test):
     if test == 0:
-        return (estimatePrice(line["s_km"], tmp0, tmp1) - line["price"])
+        return (estimatePrice(line["km"], tmp0, tmp1) - line["price"])
     else:
         return (estimatePrice(line["km"], tmp0, tmp1) - line["price"])
 
 # Pour le teta 1 il faut multiplier la marge d'erreur par le mileage par rapport a l'influence qu'il a sur la courbe
 def calculate_err_1(line, tmp0, tmp1):
-    return ((estimatePrice(line["s_km"], tmp0, tmp1) - line["price"]) * line["s_km"])
+    return ((estimatePrice(line["km"], tmp0, tmp1) - line["price"]) * line["s_km"])
 
 def calcute_sum_m(tmp0, tmp1, datas):
     tab0 = []
@@ -96,7 +96,7 @@ def ft_linear_regression(datas, teta0, teta1):
     standard_deviation_mileage = math.sqrt(calculate_v(mean_mileage, datas["km"], len(datas["km"])))
     datas["s_km"] = datas["km"].apply(standardize_column, args=(standard_deviation_mileage, mean_mileage))
     # On defini un learning rate de depart pour faire des pas plus ou moins grand sur la courbe d'erreur
-    learning_rate = 0.000001
+    learning_rate = 0.46
     nb_iterations = 50
     weaker_err = calculate_mae(tmp0, tmp1, datas)
     print("werror", weaker_err)
@@ -111,29 +111,32 @@ def ft_linear_regression(datas, teta0, teta1):
         # On peu maintenant recalculer teta0 et teta1
         tmp0 = tmp0 - learning_rate * sums_m[0]
         tmp1 = tmp1 - learning_rate * sums_m[1]
+
+        #Je compares mes erreurs avec les tetas destandardise
         s_tmp1 = destandardise_teta1(tmp1, standard_deviation_mileage)
         s_tmp0 = destandardise_teta0(tmp0, standard_deviation_mileage, mean_mileage, s_tmp1) 
-        new_err = calculate_mae(s_tmp0, s_tmp1, datas)
-        print("Moyenne d'erreur:",  new_err)
+        new_err = calculate_mae(tmp0, tmp1, datas)
+        print("Moyenne d'erreur:",  new_err, "/nteta0:", tmp0, ",teta1:", tmp1)
         #print("new err", new_err)
         if weaker_err > new_err:
             weaker_err = new_err
-            new0 = s_tmp0
-            new1 = s_tmp1
+            new0 = tmp0
+            new1 = tmp1
             savedi = i
-        datas["estimated price"] = datas["km"].apply(estimatePrice, args=(s_tmp0, s_tmp1))
+        datas["estimated price"] = datas["km"].apply(estimatePrice, args=(tmp0, tmp1))
         visusalize(datas)
         i += 1
     print(weaker_err, " ", savedi)
     datas["estimated price"] = datas["km"].apply(estimatePrice, args=(new0, new1))
     print(datas, "/nNormailized tetas:", new0, " ", new1)
+    #visusalize(datas)
 def main():
     # Recuperation des donnees dans le csv
     datas = None
     tetas = None
 
     try:
-        datas = pd.read_csv("data.csv")
+        datas = pd.read_csv("test.csv")
     except Exception as e:
         print("Something went wrong with the csv file:", str(e))
         return
